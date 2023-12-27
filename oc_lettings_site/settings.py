12743773a6ad9aa1523391401073cd0f79174335
Mathieu Cazenave <nunespace@gmail.com>
2023-12-27 17:23:32 +0100

    regul DSN

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -24,7 +24,9 @@ IS_HEROKU = "DYNO" in os.environ
 
 try:
     sentry_sdk.init(
-        dsn=os.environ["DSN"],
+        #dsn=os.environ["DSN"],
+        dsn=os.getenv["DSN"],
+
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         traces_sample_rate=1.0,
